128dd305dec9105777157a238258d73f164d85a5
刘忍 <liuren6@gmail.com>
2018-06-23 10:39:00 +0800

    更新博文

---
+++ An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/An0nym6.github.io/update.sh
@@ -0,0 +1,14 @@
+# 自动部署脚本
+
+# 编译网站
+jekyll build
+mv _site/archives.html _site/archives/index.html
+cd An0nym6.github.io
+rm -rf *
+cd ..
+cp -r _site/. An0nym6.github.io
+
+# 上传网站到 GitHub Pages
+cd An0nym6.github.io
+git add -A
+git add -A && git commit -m "更新博文"
